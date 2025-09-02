d9115df1b0f2df13cef0b3d97ee8c1d175533994
Shelby Norris <shelbs9109@gmail.com>
2025-09-02 18:50:17 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <header><h1></h1></header>
+  <main></main>
+  <footer></footer>
+
+    </>
+  )
+}
+
+export default App
