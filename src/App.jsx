128bd645c9d893fb518bc48740c9888f2ecff7ff
Shelby Norris <shelbs9109@gmail.com>
2025-09-04 19:33:59 -0500

    added MUI npm for modal, added modal to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { useState, useEffect } from "react";
 import "./App.scss";
 import { createItem, listAllItems } from "./utils/dynamo.js";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
 
 function App() {
   const [pets, setPets] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   useEffect(() => {
     (async () => {
@@ -12,6 +29,9 @@ function App() {
       setPets(items);
     })();
   }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const handleAddPet = async (event) => {
     event.preventDefault();
@@ -71,6 +91,35 @@ function App() {
                 })}
             </div>
           )}
+
+          <Button onClick={handleOpen}>Update</Button>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Pet Info
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                <form onSubmit={(event) => handleAddPet(event)}>
+                  <label htmlFor="">Name</label>
+                  <input type="text" name="petName" id="petName" />
+                  <br />
+                  <label htmlFor="">Age</label>
+                  <input type="number" name="petAge" id="petAge" />
+                  <br />
+
+                  <label htmlFor="">Adopted</label>
+                  <input type="checkbox" name="isAdopted" id="isAdopted" />
+                  <br />
+                  <button type="submit">Update</button>
+                </form>
+              </Typography>
+            </Box>
+          </Modal>
         </section>
       </main>
       <footer></footer>
